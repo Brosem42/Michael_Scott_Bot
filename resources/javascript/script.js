bb12d2b350eb0a3cb0fc3391a7740e626bcac123
Brianna Mitchell <112008058+Brosem42@users.noreply.github.com>
2023-01-12 10:48:00 -0500

    Add shuffle algo and swap

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -4,3 +4,23 @@ let rewardDoorsPicked = 0;
 let botPicked = false;
 let winningDoor = false;
 
+// Shuffle the elements of the doors array using the Fisher-Yates algorithm
+const shuffle = (array) => {
+  let currentIndex = array.length;
+  let temporaryValue;
+  let randomIndex;
+
+  // Remaining elements to shuffle
+  while (currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+};
+
